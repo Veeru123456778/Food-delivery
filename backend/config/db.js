b2c58be0f7ac6ae7945ebe5518f8bbcbc2a2f40e
Varun Kumar <kumarvarunn656@gmail.com>
2024-05-14 17:56:08 +0530

    change to db file

--- backend/config/db.js
+++ backend/config/db.js
@@ -3,5 +3,5 @@ import mongoose from 'mongoose';
 
 //FOOD-APP project name by choice 
 export const connectDb= async ()=>{
-    mongoose.connect("mongodb+srv://varun:varun123@cluster0.kdtcp6c.mongodb.net/FOOD-APP").then(()=>console.log("Database connected successfully"));
+    mongoose.connect(process.env.MONGO_URI).then(()=>console.log("Database connected successfully"));
 }
